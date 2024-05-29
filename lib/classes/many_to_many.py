24ddeaa0bb637cf254d1a49d0f9bd2e2085fa69c
Nelson Mbau <mbau.ndungu@gmail.com>
2024-05-29 08:40:08 +0300

    feat: add classes

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -3,36 +3,43 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
-        
+
+        author.articles_list.append(self)
+        magazine.article_list.append(self)
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self.articles_list = []
 
     def articles(self):
-        pass
+        return self.articles_list
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles_list]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list(set([article.magazine.category for article in self.articles_list]))
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self.article_list = []
 
     def articles(self):
-        pass
+        return self.article_list
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.article_list]))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self.article_list]
 
     def contributing_authors(self):
-        pass
+        authors_count = {}
+        return [author for author, count in authors_count.items() if count > 2]
+
